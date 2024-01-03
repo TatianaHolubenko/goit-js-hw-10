c2bd58fe92050b3b4153a1c01136f19a1f653ead
[TatianaHolubenko] <tatiana.holubenko01@gmail.com>
2024-01-03 23:55:08 +0200

    add

---
+++ src/js/2-snackbar.js
@@ -0,0 +1,36 @@
+import iziToast from 'izitoast';
+import 'izitoast/dist/css/iziToast.min.css';
+
+const form = document.querySelector('.form');
+
+form.addEventListener('submit', event => {
+  event.preventDefault();
+
+  const delayInput = document.querySelector('input[name = "delay"]');
+  const state = document.querySelector('input[name="state"]:checked').value;
+  const delay = parseInt(delayInput.value);
+
+  const createPromise = new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (state === 'fulfilled') {
+        resolve(delay);
+      } else {
+        reject(delay);
+      }
+    }, delay);
+  });
+
+  createPromise
+    .then(delay => {
+      iziToast.success({
+        title: 'Success',
+        message: `✅ Fulfilled promise in ${delay}ms`,
+      });
+    })
+    .catch(delay => {
+      iziToast.error({
+        title: 'Error',
+        message: `❌ Rejected promise in ${delay}ms`,
+      });
+    });
+});
